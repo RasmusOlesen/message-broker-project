f24aa8a2d5de39ab008f5c047480b637c66a5688
raol <raol@stibosystems.com>
2021-07-23 09:12:41 +0200

    initial commit

---
+++ message-broker-core/src/main/java/rasmus/olesen/message/broker/core/MessageBrokerCoreApplication.java
@@ -0,0 +1,13 @@
+package rasmus.olesen.message.broker.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageBrokerCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageBrokerCoreApplication.class, args);
+    }
+
+}
---
+++ message-broker-core/src/test/java/rasmus/olesen/message/broker/core/MessageBrokerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package rasmus.olesen.message.broker.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageBrokerCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
