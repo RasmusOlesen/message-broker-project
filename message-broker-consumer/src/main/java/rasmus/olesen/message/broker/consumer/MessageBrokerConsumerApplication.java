39248e365bf66a39805fe296264259b8fded965f
Rasmus Olesen <rasmusolesendk@gmail.com>
2021-07-23 10:27:44 +0200

    while true sleep

--- message-broker-consumer/src/main/java/rasmus/olesen/message/broker/consumer/MessageBrokerConsumerApplication.java
+++ message-broker-consumer/src/main/java/rasmus/olesen/message/broker/consumer/MessageBrokerConsumerApplication.java
@@ -1,13 +1,30 @@
 package rasmus.olesen.message.broker.consumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class MessageBrokerConsumerApplication {
+public class MessageBrokerConsumerApplication implements CommandLineRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(MessageBrokerConsumerApplication.class);
 
     public static void main(String[] args) {
+        LOG.info("Starting Application");
         SpringApplication.run(MessageBrokerConsumerApplication.class, args);
+        LOG.info("Stopping Application");
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("Starting CommandLineRunner");
+        System.out.print("Message Broker Consumer Application is now running.");
+        final Scanner scanner = new Scanner(System.in);
+        while (true) {
+            Thread.sleep(1000);
+        }
+    }
 }
--- message-broker-producer/src/main/java/rasmus/olesen/message/broker/producer/MessageBrokerProducerApplication.java
+++ message-broker-producer/src/main/java/rasmus/olesen/message/broker/producer/MessageBrokerProducerApplication.java
@@ -1,13 +1,30 @@
 package rasmus.olesen.message.broker.producer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class MessageBrokerProducerApplication {
+public class MessageBrokerProducerApplication implements CommandLineRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(MessageBrokerProducerApplication.class);
 
     public static void main(String[] args) {
+        LOG.info("Starting Application");
         SpringApplication.run(MessageBrokerProducerApplication.class, args);
+        LOG.info("Stopping Application");
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("Starting CommandLineRunner");
+        System.out.print("Message Broker Producer Application is now running.");
+        final Scanner scanner = new Scanner(System.in);
+        while (true) {
+            Thread.sleep(1000);
+        }
+    }
 }
